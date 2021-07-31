ba52d2d5f78f2f35d9cbdb9a9bc3ed708559c70b
Miroslav <miroslavbelov8@gmail.com>
2021-08-01 01:02:03 +0300

    [MAZE-0014] add some constants to MazeCell.hh

--- include/engine/MazeCell.hh
+++ include/engine/MazeCell.hh
@@ -35,6 +35,51 @@ namespace miroslavbel_maze_game_gdi {
          typedef uint8_t MazeCell;
 
          /*!
+          * \brief У клетки присутствует верхняя стенка.
+          */
+         constexpr MazeCell TOP_WALL = 0b1 << 0;
+
+         /*!
+          * \brief У клетки присутствует правая стенка.
+          */
+         constexpr MazeCell RIGHT_WALL = 0b1 << 1;
+
+         /*!
+          * \brief У клетки присутствует нижняя стенка.
+          */
+         constexpr MazeCell BOTTOM_WALL = 0b1 << 2;
+
+         /*!
+          * \brief У клетки присутствует левая стенка.
+          */
+         constexpr MazeCell LEFT_WALL = 0b1 << 3;
+
+         /*!
+          * \brief У клетки присутствуют все четыре стенки.
+          */
+         constexpr MazeCell ALL_WALLS = TOP_WALL | RIGHT_WALL | BOTTOM_WALL | LEFT_WALL;
+
+         /*!
+          * \brief Генератор пришел в клетку снизу.
+          */
+         constexpr MazeCell VISITED_FROM_BOTTOM = 0b1 << 4;
+
+         /*!
+          * \brief Генератор пришел в клетку слева.
+          */
+         constexpr MazeCell VISITED_FROM_LEFT = 0b1 << 5;
+
+         /*!
+          * \brief Генератор пришел в клетку сверху.
+          */
+         constexpr MazeCell VISITED_FROM_TOP = 0b1 << 6;
+
+         /*!
+          * \brief Генератор пришел в клетку справа.
+          */
+         constexpr MazeCell VISITED_FROM_RIGHT = 0b1 << 7;
+
+         /*!
           * \brief Посетил ли генератор эту клетку?
           * 
           * \param[in] cell клетка лабиринта
